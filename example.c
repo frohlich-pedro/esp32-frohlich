1452a0e299f04f694b1eb54dd0d6658065df0c27
Pedro Fröhlich <pedro_h_nf@outlook.com>
2025-10-08 11:53:54 -0300

    Update example.c

--- example.c
+++ example.c
@@ -1,8 +1,26 @@
 #include "gpio/gpio.h"
 
-#define PIN 13
-
 void app_main(void) {
-  init(PIN, OUTPUT);
-  set(PIN, HIGH);
+  gpio_t led = {
+    .pin = 13,
+    .mode = OUTPUT,
+    .state = LOW
+  };
+  
+  gpio_t button = {
+    .pin = 12,
+    .mode = INPUT_PULLUP,
+    .state = HIGH
+  };
+  
+  gpio_op[OP_INIT](&led, NULL);
+  gpio_op[OP_INIT](&button, NULL);
+
+  while (true) {
+    gpio_op[OP_READ](&button, NULL);
+  
+    if (!(button.state == HIGH)) {
+      gpio_op[OP_TOGGLE](&led, NULL);
+    }
+  }
 }
